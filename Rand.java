4bb9a77d598132543fcbe655007b4865a2f29c5e
dhruthi0708 <csridhruthi07@gmail.com>
2026-09-20 23:37:03 +0530

    Delete Rand.java

--- Rand.java
+++
@@ -1,6 +0,0 @@
-/* Rand - own Linear Congruential Generator (no java.util.Random). */
-class Rand {
-    long s;
-    Rand(long seed) { s = seed; }
-    int next(int n) { s = s * 6364136223846793005L + 1442695040888963407L; int v = (int) ((s >>> 33) % n); return v < 0 ? -v : v; }
-}
