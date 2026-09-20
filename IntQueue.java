058370f88aa04518cfdf18947094854f8af1a3fc
dhruthi0708 <csridhruthi07@gmail.com>
2026-09-20 23:34:47 +0530

    Delete IntQueue.java

--- IntQueue.java
+++
@@ -1,8 +0,0 @@
-/* IntQueue - array based queue used by BFS in Edmonds-Karp. */
-class IntQueue {
-    int[] a; int head = 0, tail = 0;
-    IntQueue(int cap) { a = new int[cap + 1]; }
-    void push(int v) { a[tail++] = v; }
-    int pop() { return a[head++]; }
-    boolean empty() { return head == tail; }
-}
